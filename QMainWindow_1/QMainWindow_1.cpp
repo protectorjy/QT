fa1b4925777d7f07b4ee9a47c0fe8c2b8123febf
欣睿 姚 <Y993077846@hotmail.com>
2021-11-28 19:35:44 +0800

    标准窗口

--- QMainWindow_1/QMainWindow_1.cpp
+++ QMainWindow_1/QMainWindow_1.cpp
@@ -3,24 +3,51 @@
 #include<QToolBar>
 #include<QDebug>
 #include<qpushbutton>
+#include<qstatusbar>
+#include<qlabel>
+#include<qdockwidget>
+#include<qtextedit>
 
 QMainWindow_1::QMainWindow_1(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
     resize(900, 700);
-    QMenuBar* bar = menuBar();
-    setMenuBar(bar);
-    QMenu * file = (*bar).addMenu("File");
-    QMenu * edit = (*bar).addMenu("edit");
-    QAction* t1 = (*file).addAction("New");
-    QAction* t2 = (*file).addAction("Open");
+    QMenuBar* mar = menuBar();
+    setMenuBar(mar);
+    QMenu* file = (*mar).addMenu("File");
+    QMenu* edit = (*mar).addMenu("Edit");
+    QMenu* view = (*mar).addMenu("View");
+    QMenu* Git = (*mar).addMenu("Git");
+    QMenu* test = (*mar).addMenu("Test");
+    (*file).addAction("New");
+    (*file).addAction("Open");
+    (*file).addAction("Clone");
+    (*file).addSeparator();
+    (*file).addAction("Quit");
     (*edit).addAction("Return");
+    (*edit).addSeparator();
+    (*edit).addAction("Copy");
+    (*edit).addAction("Stick");
+    (*view).addAction("Out");
+    (*Git).addAction("Clone in");
+    (*Git).addAction("Clone out");
+    (*Git).addAction("Synchronization");
+    (*test).addAction("Tese");
+    (*test).addAction("Run");
     QToolBar* tar = new QToolBar(this);
     addToolBar(Qt::LeftToolBarArea,tar);
-    (*tar).addAction(t1);
-    (*tar).addSeparator();
-    (*tar).addAction(t2);
-    QPushButton* b1 = new QPushButton("fuck", this);
-    (*tar).addWidget(b1);
+    (*tar).addAction("1");
+    (*tar).addAction("2");
+    (*tar).addAction("3");
+    (*tar).addAction("4");
+    (*tar).addAction("5");
+    QStatusBar* sar = statusBar();
+    setStatusBar(sar);
+    QLabel* lable = new QLabel("warning", this);
+    (*sar).addWidget(lable);
+    QDockWidget* dock = new QDockWidget("window_1", this);
+    addDockWidget(Qt::BottomDockWidgetArea, dock);
+    QTextEdit* text = new QTextEdit(this);
+    setCentralWidget(text);
 }
