b41902fde92615bedf1ba84c818b4a7a1dbb576a
欣睿 姚 <Y993077846@hotmail.com>
2021-12-09 14:52:20 +0800

    作业二阶段

--- tictactoe/algorithm.cpp
+++ tictactoe/algorithm.cpp
@@ -8,13 +8,114 @@ QString image_computer = ":/new/prefix1/resources1/20211208191706.png";
 
 void Mainsence::algorithm()        //Ëã·¨
 {
-    Change1_1(image_computer);
-    Change1_2(image_computer);
-    Change1_3(image_computer);
-    Change2_1(image_computer);
-    Change2_2(image_computer);
-    Change2_3(image_computer);
-    Change3_1(image_computer);
-    Change3_2(image_computer);
-    Change3_3(image_computer);
+    //µçÄÔ
+    if (number[0][0] == 2 && number[0][1] == 2 && number[0][2] != 1 && number[0][2] != 2)
+        Change1_3(image_computer);
+    else if (number[0][0] == 2 && number[0][2] == 2 && number[0][1] != 1 && number[0][1] != 2)
+        Change1_2(image_computer);
+    else if (number[0][1] == 2 && number[0][2] == 2 && number[0][0] != 1 && number[0][0] != 2)
+        Change1_1(image_computer);
+    else if (number[1][0] == 2 && number[1][1] == 2 && number[1][2] != 1 && number[1][2] != 2)
+        Change2_3(image_computer);
+    else if (number[1][0] == 2 && number[1][2] == 2 && number[1][1] != 1 && number[1][1] != 2)
+        Change2_2(image_computer);
+    else if (number[1][1] == 2 && number[1][2] == 2 && number[1][0] != 1 && number[1][0] != 2)
+        Change2_1(image_computer);
+    else if (number[2][0] == 2 && number[2][1] == 2 && number[2][2] != 1 && number[2][2] != 2)
+        Change3_3(image_computer);
+    else if (number[2][0] == 2 && number[2][2] == 2 && number[2][1] != 1 && number[2][1] != 2)
+        Change3_2(image_computer);
+    else if (number[2][1] == 2 && number[2][2] == 2 && number[2][0] != 1 && number[2][0] != 2)
+        Change3_1(image_computer);
+    else if (number[0][0] == 2 && number[1][0] == 2 && number[2][0] != 1 && number[2][0] != 2)
+        Change3_1(image_computer);
+    else if (number[0][0] == 2 && number[2][0] == 2 && number[1][0] != 1 && number[1][0] != 2)
+        Change2_1(image_computer);
+    else if (number[1][0] == 2 && number[2][0] == 2 && number[0][0] != 1 && number[0][0] != 2)
+        Change1_1(image_computer);
+    else if (number[0][1] == 2 && number[1][1] == 2 && number[2][1] != 1 && number[2][1] != 2)
+        Change3_2(image_computer);
+    else if (number[0][1] == 2 && number[2][1] == 2 && number[1][1] != 1 && number[1][1] != 2)
+        Change2_2(image_computer);
+    else if (number[1][1] == 2 && number[2][1] == 2 && number[0][1] != 1 && number[0][1] != 2)
+        Change1_2(image_computer);
+    else if (number[0][2] == 2 && number[1][2] == 2 && number[2][2] != 1 && number[2][2] != 2)
+        Change3_3(image_computer);
+    else if (number[0][2] == 2 && number[2][2] == 2 && number[1][2] != 1 && number[1][2] != 2)
+        Change2_3(image_computer);
+    else if (number[1][2] == 2 && number[2][2] == 2 && number[0][2] != 1 && number[0][2] != 2)
+        Change1_3(image_computer);
+    else if (number[0][0] == 2 && number[1][1] == 2 && number[2][2] != 1 && number[2][2] != 2)
+        Change3_3(image_computer);
+    else if (number[0][0] == 2 && number[2][2] == 2 && number[1][1] != 1 && number[1][1] != 2)
+        Change2_2(image_computer);
+    else if (number[1][1] == 2 && number[2][2] == 2 && number[0][0] != 1 && number[0][0] != 2)
+        Change1_1(image_computer);
+    else if (number[0][2] == 2 && number[1][1] == 2 && number[2][0] != 1 && number[2][0] != 2)
+        Change3_1(image_computer);
+    else if (number[0][2] == 2 && number[2][0] == 2 && number[1][1] != 1 && number[1][1] != 2)
+        Change2_2(image_computer);
+    else if (number[1][1] == 2 && number[2][0] == 2 && number[0][2] != 1 && number[0][2] != 2)
+        Change1_3(image_computer);
+    //Íæ¼Ò
+    else if (number[0][0] == 1 && number[0][1] == 1 && number[0][2] != 1 && number[0][2] != 2)
+        Change1_3(image_computer);
+    else if (number[0][0] == 1 && number[0][2] == 1 && number[0][1] != 1 && number[0][1] != 2)
+        Change1_2(image_computer);
+    else if (number[0][1] == 1 && number[0][2] == 1 && number[0][0] != 1 && number[0][0] != 2)
+        Change1_1(image_computer);
+    else if (number[1][0] == 1 && number[1][1] == 1 && number[1][2] != 1 && number[1][2] != 2)
+        Change2_3(image_computer);
+    else if (number[1][0] == 1 && number[1][2] == 1 && number[1][1] != 1 && number[1][1] != 2)
+        Change2_2(image_computer);
+    else if (number[1][1] == 1 && number[1][2] == 1 && number[1][0] != 1 && number[1][0] != 2)
+        Change2_1(image_computer);
+    else if (number[2][0] == 1 && number[2][1] == 1 && number[2][2] != 1 && number[2][2] != 2)
+        Change3_3(image_computer);
+    else if (number[2][0] == 1 && number[2][2] == 1 && number[2][1] != 1 && number[2][1] != 2)
+        Change3_2(image_computer);
+    else if (number[2][1] == 1 && number[2][2] == 1 && number[2][0] != 1 && number[2][0] != 2)
+        Change3_1(image_computer);
+    else if (number[0][0] == 1 && number[1][0] == 1 && number[2][0] != 1 && number[2][0] != 2)
+        Change3_1(image_computer);
+    else if (number[0][0] == 1 && number[2][0] == 1 && number[1][0] != 1 && number[1][0] != 2)
+        Change2_1(image_computer);
+    else if (number[1][0] == 1 && number[2][0] == 1 && number[0][0] != 1 && number[0][0] != 2)
+        Change1_1(image_computer);
+    else if (number[0][1] == 1 && number[1][1] == 1 && number[2][1] != 1 && number[2][1] != 2)
+        Change3_2(image_computer);
+    else if (number[0][1] == 1 && number[2][1] == 1 && number[1][1] != 1 && number[1][1] != 2)
+        Change2_2(image_computer);
+    else if (number[1][1] == 1 && number[2][1] == 1 && number[0][1] != 1 && number[0][1] != 2)
+        Change1_2(image_computer);
+    else if (number[0][2] == 1 && number[1][2] == 1 && number[2][2] != 1 && number[2][2] != 2)
+        Change3_3(image_computer);
+    else if (number[0][2] == 1 && number[2][2] == 1 && number[1][2] != 1 && number[1][2] != 2)
+        Change2_3(image_computer);
+    else if (number[1][2] == 1 && number[2][2] == 1 && number[0][2] != 1 && number[0][2] != 2)
+        Change1_3(image_computer);
+    else if (number[0][0] == 1 && number[1][1] == 1 && number[2][2] != 1 && number[2][2] != 2)
+        Change3_3(image_computer);
+    else if (number[0][0] == 1 && number[2][2] == 1 && number[1][1] != 1 && number[1][1] != 2)
+        Change2_2(image_computer);
+    else if (number[1][1] == 1 && number[2][2] == 1 && number[0][0] != 1 && number[0][0] != 2)
+        Change1_1(image_computer);
+    else if (number[0][2] == 1 && number[1][1] == 1 && number[2][0] != 1 && number[2][0] != 2)
+        Change3_1(image_computer);
+    else if (number[0][2] == 1 && number[2][0] == 1 && number[1][1] != 1 && number[1][1] != 2)
+        Change2_2(image_computer);
+    else if (number[1][1] == 1 && number[2][0] == 1 && number[0][2] != 1 && number[0][2] != 2)
+        Change1_3(image_computer);
+    else
+    {
+        Change1_1(image_computer);
+        Change1_2(image_computer);
+        Change1_3(image_computer);
+        Change2_1(image_computer);
+        Change2_2(image_computer);
+        Change2_3(image_computer);
+        Change3_1(image_computer);
+        Change3_2(image_computer);
+        Change3_3(image_computer);
+    }
 }
